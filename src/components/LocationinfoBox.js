1372e35f312deb0cd443bc4e948ec6b8eb77ae86
apurva-modi <amodi002@odu.edu>
2020-12-07 15:53:37 -0500

    Completed wildFire tracker using NASA-API

---
+++ src/components/LocationinfoBox.js
@@ -0,0 +1,15 @@
+const LocationinfoBox = ({info}) => {
+    
+    return (
+
+        <div className="location__info" >
+            <h2> Event Location Info</h2>
+            <ul>
+                <li> ID: <strong>{info.id}</strong></li>
+                <li>TITLE: <strong>{info.title}</strong></li>
+            </ul>
+        </div>
+    )
+}
+
+export default LocationinfoBox
